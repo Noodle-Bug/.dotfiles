e1958419546feaaa896a62aa36085def0a550f43
NoodleBug <63314982+Noodle-Bug@users.noreply.github.com>
2024-03-10 01:32:37 -0600

    Adding weather to eww bar

--- .config/eww/scripts/weather/getWeather.sh
+++ .config/eww/scripts/weather/getWeather.sh
@@ -2,4 +2,6 @@ curl \
 	-H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0' \
 	'https://api.weather.gov/gridpoints/AKQ/37,93/forecast?units=us' > weather.json
 
-cat weather.json | jq '.properties.periods[0] | {temperature, icon, shortForecast, detailedForecast}' > parsedWeather.json
+# cat weather.json | jq '.properties.periods[0] | {temperature, icon, shortForecast, detailedForecast}' > parsedWeather.json
+cat weather.json | jq '.properties.periods[0] | .temperature' > temperature.txt
+cat temperature.txt
